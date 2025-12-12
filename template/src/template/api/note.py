21a3a51e817835ed617b0910f9253688f5a3cda9
Artem Burenin <burenin@mindbox.cloud>
2025-12-12 21:49:06 +0300

    Add app

--- template/src/template/api/note.py
+++ template/src/template/api/note.py
@@ -1,13 +1,14 @@
 from typing import Annotated
 from uuid import UUID
-from fastapi import APIRouter, Path
-from commons.auth import create_authorization_guard
+
+from commons.auth import Permission, UserClaims, create_authorization_guard
+from fastapi import APIRouter, Depends, Path
 
 notes_router = APIRouter(prefix="/notes")
 
 authorize = create_authorization_guard(
-    token_url="http://localhost:8080/token_url", 
-    verification_key="", 
+    token_url="http://localhost:8080/token_url",
+    verification_key="",
     algorithms=["HS256"],
 )
 
@@ -15,5 +16,5 @@ authorize = create_authorization_guard(
 @notes_router.post("/{id}")
 async def create_note(
     id: Annotated[UUID, Path()],
-    user: Annotated[UserClaims, create_authorization_guard]
-) -> 
+    user: Annotated[UserClaims, Depends(authorize({Permission.CREATE_NOTE}))],
+) -> None: ...
